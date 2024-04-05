506bda6f3ac186a22a5c4330903dd71d48f3d6a4
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-04-05 18:20:46 +0100

    Moved page components to src/components

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './HomeScreen';
 import CameraScreen from './CameraScreen'; 
-import ChatScreen from './ChatScreen';
+import ChatScreen from './src/components/ChatScreen';
 import { StyleSheet, View, Text, TouchableOpacity, StatusBar } from 'react-native';
 
 const Stack = createNativeStackNavigator();
